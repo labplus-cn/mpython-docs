516e61819624eaf6809c40261d8af35cdd9619c2
曾立举 <957790617@qq.com>
2023-11-17 18:04:21 +0800

    更新

--- docs/conf_common.py
+++ docs/conf_common.py
@@ -16,7 +16,7 @@ import sys
 import os
 import shlex
 import datetime
-import urllib
+import urllib.request
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
